49bd19c5bf5459b88c70c7c80663ad5dec41d283
aquelePablo <74788322+aquelepablo@users.noreply.github.com>
2026-08-25 12:47:39 +0200

    feat(F0004.1): add OAuthController exposing POST /oauth/token

---
+++ back-end/ONG.API/Controllers/OAuthController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using ONG.Application.UseCases.OAuth.IssueClientToken;
+
+namespace ONG.API.Controllers
+{
+    [ApiController]
+    [Route("oauth")]
+    public class OAuthController : ControllerBase
+    {
+        private readonly IssueClientTokenHandler _handler;
+
+        public OAuthController(IssueClientTokenHandler handler)
+        {
+            _handler = handler;
+        }
+
+        [HttpPost("token")]
+        public IActionResult Token(IssueClientTokenCommand command)
+        {
+            var result = _handler.Handle(command);
+
+            if (result is null)
+            {
+                return Unauthorized(new { message = "Invalid client_id or client_secret." });
+            }
+
+            return Ok(result);
+        }
+    }
+}
